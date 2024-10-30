beeb3c15677d71e2bae279562e8ee9a0e4b72dcf
RickM19 <riquelmo14br@gmail.com>
2024-10-30 19:50:26 -0300

    feat: create error handler

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,17 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import express from 'express';
 import sequelize from '../../config/sequelize';
 import cors from 'cors';
 import 'express-async-errors';
+import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/healthcheck', (req, res) => {
+app.get('/healthcheck', (_req, res) => {
     res.status(200).send({ message: 'Server is up and running!' });
 });
 
+app.use(errorHandler);
 const startServer = async () => {
     try {
         await sequelize.authenticate();
