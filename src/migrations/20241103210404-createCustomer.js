e30683562b9f9d315da5a12e3b6d40b9da7bdf26
Johnathan Maia <jhoosnow27@gmail.com>
2024-11-03 18:10:12 -0300

    feat:Add migration files configurations

--- src/migrations/20241103210404-createCustomer.js
+++ src/migrations/20241103210404-createCustomer.js
@@ -1,22 +1,61 @@
 'use strict';
+/* eslint-disable */
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('customers', {
+            id: {
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4,
+                primaryKey: true,
+                allowNull: false,
+            },
+            fullName: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            birthDate: {
+                type: Sequelize.DATEONLY,
+                allowNull: false,
+            },
+            cpf: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            phone: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            registrationDate: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.NOW,
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.NOW,
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.NOW,
+            },
+            deletedAt: {
+                type: Sequelize.DATE,
+                allowNull: true,
+            },
+        });
+    },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('customers');
+    },
 };
